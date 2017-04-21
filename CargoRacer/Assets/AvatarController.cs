b33c370a75cdd467a375552fc486fe4ecbb8ec77
antomeu <anto.meu@gmail.com>
2017-04-21 19:56:23 +0200

    Second commit

---
+++ CargoRacer/Assets/AvatarController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarController : MonoBehaviour
+{
+
+    #region SET IN UNITY
+    public float LaneWidth = 6f;
+    public float LaneChangingSpeed = 6f;
+    #endregion
+
+    private float[] LaneCoordinates = new float[4];
+    public int CurrentLane = 2;
+    public int movingDirection;
+    public bool hasReachedLane;
+
+    // Use this for initialization
+    void Start () {
+        for (int i = 0; i <= 3; i++)
+        {
+            LaneCoordinates[i] = LaneWidth * (i - 3/2) - 3; //LaneWidth /2*((i - 2) + 1);
+        }
+
+        CurrentLane = 2;
+        transform.position = LaneCoordinates[CurrentLane] * Vector3.right;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (Input.GetKeyDown(KeyCode.LeftArrow) )
+	    {
+	        movingDirection = -1;
+            CurrentLane += movingDirection;
+        }
+
+	    if (Input.GetKeyDown(KeyCode.RightArrow) )
+	    {
+	        movingDirection = 1;
+            CurrentLane += movingDirection;
+        }
+
+	    //if (CurrentLane > 0 && CurrentLane < LaneCoordinates.Length && !hasReachedLane)
+	    //{
+            MoveAvatar();
+        //}
+    }
+
+    void MoveAvatar()
+    {
+        if (Math.Abs(transform.position.x - LaneCoordinates[CurrentLane]) > 0.1f)
+        {
+            transform.position += ((movingDirection * LaneChangingSpeed) * Time.deltaTime) * Vector3.right;
+            transform.rotation = Quaternion.Euler(0,movingDirection * 8f ,0);
+            Debug.Log(LaneCoordinates[CurrentLane]);
+        }
+        else
+        {
+            hasReachedLane = true;
+            transform.position = LaneCoordinates[CurrentLane] * Vector3.right;
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
+    }
+}
---
+++ CargoRacer/Assets/WorldController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour {
+
+    #region Set in Unity
+
+    public float Speed;
+    public float TrafficSpeed = 10;
+    public Transform OppositeTraffic;
+    public Transform SameWayTraffic;
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    transform.position += Speed * Time.deltaTime * Vector3.back;
+        OppositeTraffic.position += TrafficSpeed * Time.deltaTime * Vector3.back;
+        SameWayTraffic.position += TrafficSpeed * Time.deltaTime * Vector3.forward;
+    }
+}
