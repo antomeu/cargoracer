11a7ab7e69be73f26b541dd2ee4d436295af5eb4
antomeu <anto_meu@yahoo.fr>
2017-04-29 21:26:51 +0200

    Quicksave

--- CargoRacer/Assets/Scripts/EndGameManager.cs
+++ CargoRacer/Assets/Scripts/EndGameManager.cs
@@ -11,40 +11,42 @@ public class EndGameManager : MonoBehaviour {
     //public InputField InputName;
     public Button ButtonRestart; // Show when player name is submitter
     public LeaderBoardsManager LeaderBoardManager;
-
+    public ConclifyApi api;
 
 	void Start ()
     {
-        ButtonRestart.enabled = false;
+        //ButtonRestart.enabled = false;
+        GetPlayersScore();
     }
 	
     public void SendName()
     {
-        ButtonRestart.enabled = true;
+        //ButtonRestart.enabled = true;
     }
-
     
-    private ConclifyApi GetApi()
-    {
-        ConclifyApi api = GetComponent<ConclifyApi>();
-        return api;
-    }
 
     void GetPlayersScore()
     {
-        ConclifyApi api = GetApi();
-
+        
         api.RequestPlayerPost();
         api.RequestPlayerPatch(Globals.PlayerName);
         api.RequestPlayerScorePost(Globals.PackagesDelivered);
 
-        Debug.Log(api.Player.Id);
+        Debug.Log(api.Player.FirstName);
 
-        for (int i = 0; i<= LeaderBoardManager.PlayerScoreManager.Length;i++)
+        int index = 1;
+        foreach (ConclifyApiGameScore gameScore in api.Game.Scores)
         {
-            string rank = gameScore.Rank; //String so it can be things like "=2"
+            string rank = gameScore.Rank; 
             string name = gameScore.Name;
-            string score = gameScore.Score; //String because it includes formatting, like thousand-separators
+            string score = gameScore.Score;
+            Debug.Log(gameScore);
+            if (index <= LeaderBoardManager.PlayerScoreManager.Length)// Only display the top of the board
+            {
+                LeaderBoardManager.PlayerScoreManager[index].SetPlayerInfo(rank, name, score);
+            }
+            index++;
+
         }
     }
 }
